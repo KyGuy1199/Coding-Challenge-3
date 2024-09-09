e5f816f57addad86801ce928560859629c040188
Kyle Thomas <kylethomas1@usf.edu>
2024-09-08 23:54:42 -0400

    Add files via upload

---
+++ sales_performance_analyzer.js
@@ -0,0 +1,24 @@
+//Task 1: Create a Function to Calculate Average Sales
+
+function CalculateAverageSales(sales) {
+    const average= array => array.reduce((total, transaction) => total + transaction, 0) / Array.length;
+    return sum / array.length
+
+    console.log(average);
+}
+// Task 2: Create a Function to Determine Performance Rating
+function determinePerformanceRating(sales) {
+    if (sales < $4000) {determinePerformanceRating = "Needs Improvement"}
+   
+    else if (sales >= $4000 && sales <= $7000) {determinePerformanceRating = "Satisfactory"}
+    
+     else if (sales >= $7000 && sales <= $10000) {determinePerformanceRating = "Good"}
+   
+         else if (sales == $100000) {determinePerformanceRating = "Excellent"}
+    
+   }
+    
+   // Task 3: Create a Function to Identify Top and Bottom Performers
+   function findTopAndBottomPerformers(salesperson) {
+    
+   }
